b29ba0edd432c2e282d0e61688f2e4e462957bdf
cuuw619-maker <cuuw619@gmail.com>
2026-09-14 21:49:01 +0300

    Fix game-core JVM target compatibility

--- game-core/build.gradle.kts
+++ game-core/build.gradle.kts
@@ -8,6 +8,15 @@ android {
     namespace = "com.imux.gamecore"
     compileSdk = 35
     defaultConfig { minSdk = 24 }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
