1c3cd3b37a29f5364de9f26b9113dac8ac70890f
cuuw619-maker <cuuw619@gmail.com>
2026-09-15 09:52:04 +0300

    Add unit test dependency for game-core

--- game-core/build.gradle.kts
+++ game-core/build.gradle.kts
@@ -25,4 +25,5 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:3.0.3")
     implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    testImplementation("junit:junit:4.13.2")
 }
