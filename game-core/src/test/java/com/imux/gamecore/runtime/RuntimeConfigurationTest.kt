4cfa6fb0cadf1a3b5364f25443cf5add4aafb8b4
cuuw619-maker <cuuw619@gmail.com>
2026-09-15 09:52:32 +0300

    Use JUnit for game-core runtime test

--- game-core/src/test/java/com/imux/gamecore/runtime/RuntimeConfigurationTest.kt
+++ game-core/src/test/java/com/imux/gamecore/runtime/RuntimeConfigurationTest.kt
@@ -1,7 +1,7 @@
 package com.imux.gamecore.runtime
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class RuntimeConfigurationTest {
     @Test
