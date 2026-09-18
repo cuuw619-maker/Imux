729ac27866a681b1aeaf8069c2b4fa75975d6649
cuuw619-maker <cuuw619@gmail.com>
2026-09-18 07:26:46 +0300

    Tidy launcher Compose imports

--- app/src/main/java/com/imux/game/launcher/screens/HomeScreen.kt
+++ app/src/main/java/com/imux/game/launcher/screens/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.imux.game.launcher.screens
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
@@ -20,7 +19,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -41,7 +39,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.imux.game.R
