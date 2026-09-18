960cf6a64854ddc40fb9b8963c160c4185e4cdb6
cuuw619-maker <cuuw619@gmail.com>
2026-09-18 07:36:12 +0300

    Fix launcher Compose state delegate import

--- app/src/main/java/com/imux/game/launcher/screens/HomeScreen.kt
+++ app/src/main/java/com/imux/game/launcher/screens/HomeScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
